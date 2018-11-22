fb6f816d3802c1b398d9b308e1556f390914f6cc
Dominik-Ubuntu <dominik224@interia.pl>
2018-11-22 21:02:58 +0000

    IT WORKS

--- Sample.c
+++ Sample.c
@@ -7,6 +7,9 @@
 #include "Polynomials/Poly.h"
 
 int main() {
-  make_polynomial(5);
+  Polynomial* poly = make_polynomial(5);
+  poly->Coefficient_Array[0] = 5;
+
+  printf("if this number -> %d <- is five it means it works!<\n", poly->Coefficient_Array[0]);
   return 0;
 }
