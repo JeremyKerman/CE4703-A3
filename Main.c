5f506073e0f18fe3a97da65207b7b7ca1e9335e6
Dominik-Ubuntu <dominik224@interia.pl>
2018-11-24 00:20:29 +0000

    added main file

---
+++ Main.c
@@ -0,0 +1,28 @@
+///////////////////////////////////////////////////////
+// Main file for group assignment 3
+// Polynomial function manipulator with user input
+//
+// Date: 22.11.2018
+// Authors: Dominik Przychodni - 17204658
+//          Matas Mockus       - G4Y80Y
+//          Eoghan McGrath     - 1DKL0L
+//          Brian McSweeney    - WITHAY
+//          Jeremy Pili        - WEARSFAKEGLASSES
+///////////////////////////////////////////////////////
+
+//include system headers
+#include <stdlib.h>
+#include <stdio.h>
+//include custom headers
+#include "Polynomials/Poly.h"
+
+/////////////////////////////////////////////////////////
+//  Main Function
+//
+//  provides an interface for the user to input values
+//  and use the software functions
+///////////////////////////////////////////////////////////
+int main() {
+  
+  return EXIT_SUCCESS;
+}
